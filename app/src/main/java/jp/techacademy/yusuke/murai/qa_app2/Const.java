d1fd369631ae798bbeb9c0a4c5dff7e9d044b85e
ym.devtech <ym.devtech@gmail.com>
2017-02-09 11:36:40 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yusuke/murai/qa_app2/Const.java
@@ -0,0 +1,13 @@
+package jp.techacademy.yusuke.murai.qa_app2;
+
+/**
+ * Created by my99workmac on 2017/02/09.
+ */
+
+public class Const {
+    public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するバス
+    public static final String AnswersPATH = "answers"; // Firebaseに解答を保存するパス
+
+    public static final String NameKEY = "name"; // Preferenceに表示名を保存する時のキー
+}
