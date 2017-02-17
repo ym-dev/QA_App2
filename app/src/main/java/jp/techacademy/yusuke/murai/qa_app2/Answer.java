e1464bab131bd48005e4e51b2ba1f8e5691edf7b
ym.devtech <ym.devtech@gmail.com>
2017-02-17 23:29:13 +0900

    8. 質問一覧画面　実装完了

---
+++ app/src/main/java/jp/techacademy/yusuke/murai/qa_app2/Answer.java
@@ -0,0 +1,33 @@
+package jp.techacademy.yusuke.murai.qa_app2;
+
+import java.io.Serializable;
+
+public class Answer implements Serializable {
+    private String mBody;
+    private String mName;
+    private String mUid;
+    private String mAnswerUid;
+
+    public Answer(String body, String name, String uid, String answerUid) {
+        mBody = body;
+        mName = name;
+        mUid = uid;
+        mAnswerUid = answerUid;
+    }
+
+    public String getBody() {
+        return mBody;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getUid() {
+        return mUid;
+    }
+
+    public String getAnswerUid() {
+        return mAnswerUid;
+    }
+}
--- app/src/main/java/jp/techacademy/yusuke/murai/qa_app2/MainActivity.java
+++ app/src/main/java/jp/techacademy/yusuke/murai/qa_app2/MainActivity.java
@@ -1,6 +1,8 @@
 package jp.techacademy.yusuke.murai.qa_app2;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -10,17 +12,113 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
     private int mGenre = 0;
+
+    // --- ここから ---
+    private DatabaseReference mDatabaseReference;
+    private DatabaseReference mGenreRef;
+    private ListView mListView;
+    private ArrayList<Question> mQuestionArrayList;
+    private QuestionsListAdapter mAdapter;
+
+    private ChildEventListener mEventListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            HashMap map = (HashMap) dataSnapshot.getValue();
+            String title = (String) map.get("title");
+            String body = (String) map.get("body");
+            String name = (String) map.get("name");
+            String uid = (String) map.get("uid");
+            String imageString = (String) map.get("image");
+            Bitmap image = null;
+            byte[] bytes;
+            if (imageString != null) {
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                bytes = Base64.decode(imageString, Base64.DEFAULT);
+            } else {
+                bytes = new byte[0];
+            }
+
+            ArrayList<Answer> answerArrayList = new ArrayList<Answer>();
+            HashMap answerMap = (HashMap) map.get("answers");
+            if (answerMap != null) {
+                for (Object key : answerMap.keySet()) {
+                    HashMap temp = (HashMap) answerMap.get((String) key);
+                    String answerBody = (String) temp.get("body");
+                    String answerName = (String) temp.get("name");
+                    String answerUid = (String) temp.get("uid");
+                    Answer answer = new Answer(answerBody, answerName, answerUid, (String) key);
+                    answerArrayList.add(answer);
+                }
+            }
+
+            Question question = new Question(title, body, name, uid, dataSnapshot.getKey(), mGenre, bytes, answerArrayList);
+            mQuestionArrayList.add(question);
+            mAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            HashMap map = (HashMap) dataSnapshot.getValue();
+
+            // 変更があったQuestionを探す
+            for (Question question: mQuestionArrayList) {
+                if (dataSnapshot.getKey().equals(question.getQuestionUid())) {
+                    // このアプリで変更がある可能性があるのは回答(Answer)のみ
+                    question.getAnswers().clear();
+                    HashMap answerMap = (HashMap) map.get("answers");
+                    if (answerMap != null) {
+                        for (Object key : answerMap.keySet()) {
+                            HashMap temp = (HashMap) answerMap.get((String) key);
+                            String answerBody = (String) temp.get("body");
+                            String answerName = (String) temp.get("name");
+                            String answerUid = (String) temp.get("uid");
+                            Answer answer = new Answer(answerBody, answerName, answerUid, (String) key);
+                            question.getAnswers().add(answer);
+                        }
+                    }
+
+                    mAdapter.notifyDataSetChanged();
+                }
+            }
+        }
+
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    };
+    // --- ここまで追加する ---
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +137,7 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
-                // ログイン済みのユーザーを取得する
+                // ログイン済みのユーザーを収録する
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
                 if (user == null) {
@@ -52,6 +150,7 @@ public class MainActivity extends AppCompatActivity {
                     intent.putExtra("genre", mGenre);
                     startActivity(intent);
                 }
+
             }
         });
 
@@ -83,14 +182,38 @@ public class MainActivity extends AppCompatActivity {
 
                 DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
                 drawer.closeDrawer(GravityCompat.START);
+
+                // --- ここから ---
+                // 質問のリストをクリアしてから再度Adapterにセットし、AdapterをListViewにセットし直す
+                mQuestionArrayList.clear();
+                mAdapter.setQuestionArrayList(mQuestionArrayList);
+                mListView.setAdapter(mAdapter);
+
+                // 選択したジャンルにリスナーを登録する
+                if (mGenreRef != null) {
+                    mGenreRef.removeEventListener(mEventListener);
+                }
+                mGenreRef = mDatabaseReference.child(Const.ContentsPATH).child(String.valueOf(mGenre));
+                mGenreRef.addChildEventListener(mEventListener);
+                // --- ここまで追加する ---
                 return true;
             }
         });
+
+        // --- ここから ---
+        // Firebase
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
+        // ListViewの準備
+        mListView = (ListView) findViewById(R.id.listView);
+        mAdapter = new QuestionsListAdapter(this);
+        mQuestionArrayList = new ArrayList<Question>();
+        mAdapter.notifyDataSetChanged();
+        // --- ここまで追加する ---
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
